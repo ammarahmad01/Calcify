7c6ad310c0fad0f086d8425e50c5d5e7fc32ecb0
Ammar Ahmad <ammar.ahmad.rwp@gmail.com>
2026-04-20 21:37:43 +0500

    Test full CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,11 @@ resource "aws_instance" "calcify_server" {
   vpc_security_group_ids      = [aws_security_group.calcify_sg.id]
   associate_public_ip_address = true
 
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "calcify-server"
   }
